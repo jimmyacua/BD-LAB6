7edaee99910ef56c0a8a500c99f1065aef37963b
Jimmy Acuña <b50060@ecci.ucr.ac.cr>
2018-10-19 16:39:38 -0600

    hasta 4 pero falta arreglar el select

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,4 +1,15 @@
 -- en sql server no existe el trigger "before", el "instead of" puede simular un before.
+/* declare c cursor for
+* select cedula from Deleted 
+* open c
+* Fetch next from c into @ced
+* while @@fetch_status = 0 begin
+*	//aquí va la lógica
+*	Fetch next from c into @ced
+*end 
+* close c //IMPORTANTE
+* deallocate c //IMPORTANTE
+*/
 
 use DB_B50060;
 
@@ -12,12 +23,11 @@ foreign key(Cedula) references Estudiante(Cedula);
 --3:
 go
 create trigger ElimEstudiante
-on Estudiante instead of delete
+on Estudiante instead of delete 
 as
 	declare @ced char(9) 
-	delete from Asistente
 	select @ced = Cedula from deleted
-	@ced = Cedula
+	delete from Asistente
 	where Cedula = @ced
 	delete from Estudiante
 	where Cedula = @ced
@@ -25,11 +35,40 @@ go
 
 select * from Asistente
 
+--agrego un estudiante que no está relacionado con nada
 insert into Estudiante
 values('1132254','gec@gmail.com','geovanny','cordero', 'valverde', 'M', '1996-02-17', 'perez zeledon', '82165431', 'B40034', 'Activo');
-
 insert into Asistente
 values('1132254', 8);
 
 delete from Estudiante
 where Cedula = '1132254'
+
+--4:
+
+go
+create trigger CierraGrupo
+on Grupo instead of delete
+as
+	declare @sigla char(7)
+	select @sigla = d.SiglaCurso from deleted d-- join Lleva l on d.SiglaCurso = l.SiglaCurso
+	declare @nG int
+	select @nG = d.NumGrupo from deleted d --join Lleva l on d.NumGrupo = l.NumGrupo
+	declare @sem int
+	select @sem = d.Semestre from deleted d --join Lleva l on d.Semestre = l.Semestre
+	declare @anno int
+	select @anno = d.Año from deleted d --join Lleva l on d.Año = l.Año
+	delete from Grupo
+	--delete from Lleva
+go
+
+drop trigger CierraGrupo
+
+select * from Lleva
+select * from Grupo
+
+delete from Grupo 
+where(SiglaCurso, NumGrupo, Semestre, Año)  
+		(Select l.SiglaCurso, l.NumGrupo, l.Semestre, l.Año
+		  from Lleva l
+		  where l.Nota is null);
