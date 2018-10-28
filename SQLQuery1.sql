76ec723050cd94a041bca8f0c35b65d392c82b0b
Jimmy <jimmyacua@hotmail.com>
2018-10-27 20:12:45 -0600

    5 se encicla

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -50,16 +50,23 @@ go
 create trigger CierraGrupo
 on Grupo instead of delete
 as
-	declare @sigla char(7), @nG int, @sem int, @anno int
+	declare @notNulls int
+	select @notNulls = count(*)
+	from deleted d join Lleva l on d.SiglaCurso = l.SiglaCurso
+	where l.Nota is not null
+
+if @notNulls = 0
+begin
+	declare @sigla char(7), @nG int, @sem int, @anno int, @notNull int
 	select @sigla = d.SiglaCurso, @nG = d.NumGrupo, @sem = d.Semestre, @anno = d.Año 
-	from deleted d join Lleva l on d.SiglaCurso = l.SiglaCurso and d.NumGrupo = l.NumGrupo and d.Semestre = l.Semestre and d.Año = l.Año
-	where 
-		 
+	from deleted d
+		
 	delete from Lleva 
-	where SiglaCurso= @sigla and NumGrupo = @nG and Semestre = @sem and Año = @anno
+	where SiglaCurso = @sigla and NumGrupo = @nG and Semestre = @sem and Año = @anno
 	
 	delete from Grupo
 	where SiglaCurso= @sigla and NumGrupo = @nG and Semestre = @sem and Año = @anno
+end
 	
 go
 
@@ -69,15 +76,27 @@ select * from Lleva
 --Solo un estudiante matriculado con nota null
 
 delete from Grupo 
+where SiglaCurso = 'ci1312' 
+
+select * from Lleva
+select * from Grupo
+
+EXEC ActualizarNotaEstudiante @ced = '99888777', @sigla = 'ci1312', @numG= 1, 
+@sem = 2, @año = 2018, @nuevaNota = null 
+
+delete from Grupo 
 where SiglaCurso = 'ci1312'
 
 select * from Lleva
 select * from Grupo
 
---4.i) Se ejecuta el trigger correctamente, pero no tiene ningún efecto pues no encuentra una nota nula en la tabla Lleva
+--4.i)
+	delete from Grupo 
+	where SiglaCurso = 'ci1312'
+	select * from Grupo
+-- Al ejecutarse se eliminar el grupo que no contiene estudiantes matriculados.
 
 --4.ii) 
-
 
 insert into Grupo values(
 'ci1312', 1, 2, 2018, '234567890', 4, '111222333'
@@ -93,23 +112,84 @@ insert into Lleva values
 ('876543219','ci1312', 1,2,2018, null)
 
 insert into Lleva values
-('99888777','ci1312', 1,2,2018, 90)
+('99888777','ci1312', 1,2,2018, null)
 
 select * from Lleva
-select * from Grupo
+where SiglaCurso = 'ci1312'
 
-EXEC ActualizarNotaEstudiante @ced = '99888777', @sigla = 'ci1312', @numG= 1, 
-@sem = 2, @año = 2018, @nuevaNota = null 
+delete from Grupo where SiglaCurso = 'ci1312'
+select * from Lleva
+where SiglaCurso = 'ci1312'
+
+--se eliminan todas las tuplas de la tabla del grupo especificado.
+
+--4.iii)
+
+insert into Grupo values(
+'ci1312', 1, 2, 2018, '234567890', 4, '111222333'
+);
+--se agregan estudiantes con nota null
+insert into Lleva values
+('111222333','ci1312', 1,2,2018, null)
+
+insert into Lleva values
+('176543219','ci1312', 1,2,2018, 13)
+
+insert into Lleva values
+('876543219','ci1312', 1,2,2018, null)
+
+insert into Lleva values
+('99888777','ci1312', 1,2,2018, 90)
+
+select * from Lleva where SiglaCurso = 'ci1312'
 
 delete from Grupo 
 where SiglaCurso = 'ci1312'
 
-select c.Codigo from Carrera c
+select * from Lleva where SiglaCurso = 'ci1312'
 
---Se borran todas las tuplas que tienen nota null
+--No se elimina el grupo porque se encuentran notas distintas a null
 
 --5:
+drop trigger CierraGrupo
+go
+create trigger CierraVariosGrupos
+on Grupo instead of delete
+as
+	declare @sigla char(7)
+	declare c cursor for 
+	select SiglaCurso from deleted
+	open c
+	Fetch next from c into @sigla
+	while @@FETCH_STATUS = 0 begin
+		declare @notNulls int
+		select @notNulls = count(*)
+		from deleted d join Lleva l on d.SiglaCurso = l.SiglaCurso
+		where l.Nota is not null
 
+		if @notNulls = 0
+		begin
+			declare @nG int, @sem int, @anno int, @notNull int
+			select @sigla = d.SiglaCurso, @nG = d.NumGrupo, @sem = d.Semestre, @anno = d.Año 
+			from deleted d
+
+			delete from Lleva 
+			where SiglaCurso = @sigla and NumGrupo = @nG and Semestre = @sem and Año = @anno
+	
+			delete from Grupo
+			where SiglaCurso= @sigla and NumGrupo = @nG and Semestre = @sem and Año = @anno
+		end
+	end
+	close c
+	deallocate c
+go
+
+
+-------------------
+select * from Lleva
+select * from Grupo
+
+delete from Grupo
 
 
 --6:
